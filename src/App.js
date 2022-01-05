b499145ae42ec4a442368207dbadff2a178a2dc9
Muharrem Dur <muharram2000dur@gmail.com>
2022-01-05 22:35:34 +0300

    tarih eklendi

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import moment from 'moment';
+import 'moment/locale/tr';
 
 function App() {
   return (
     <div className="App">
      <p>Hello React!</p>
+     <h2>Tarih: </h2>
+      <p> {moment().format('MMMM Do YYYY, h:mm:ss a')}</p>
     </div>
   );
 }
